a1d2c643917133ff6e5f7b3d87eeb1f5a7ec9154
Eduardo Mello <eduardormello@gmail.com>
2022-09-20 17:38:17 -0300

    feat: ✨ omit logs from output console (#2)

--- pkg/core/core_test.go
+++ pkg/core/core_test.go
@@ -322,10 +322,10 @@ func TestVersion(t *testing.T) {
 func TestParsePattern(t *testing.T) {
 	patterns := make(map[string]int)
 	//Logging output should be tested here
-	parsePatterns(patterns)
+	parsePatterns(patterns, &Config{})
 	patterns2 := make(map[string]interface{})
 	patterns2["test"] = patterns
-	parsePatterns(patterns2)
+	parsePatterns(patterns2, &Config{})
 }
 
 func TestAnalyseDom(t *testing.T) {
@@ -334,7 +334,7 @@ func TestAnalyseDom(t *testing.T) {
 	detectedApp := &detected{}
 	app.Dom = false
 	//Logging output should be tested here
-	analyzeDom(app, godoc, detectedApp)
+	analyzeDom(app, godoc, detectedApp, &Config{})
 }
 
 func TestRecursivity(t *testing.T) {
